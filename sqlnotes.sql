3558e292effcfc173d61c6896e1b5710e00a32b2
Alem Haddush Fitwi <fitwialem2022@fb.com>
2023-01-16 23:49:46 -0800

    Create sqlnotes.sql

---
+++ sqlnotes.sql
@@ -0,0 +1,9 @@
+;A. ALTER TABLE lets you add column to a table in db
+;-----------------------------------------------------------------------
+ALTER TABLE test
+ADD r6 TEXT;
+;-----------------------------------------------------------------------
+; B. AND is an operator that combines two conditions. Both conditions must
+;    be true for the row to be included in the result set.
+
+;-----------------------------------------------------------------------
