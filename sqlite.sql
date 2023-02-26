0b3fc66c213751549277f8dba2bc21194b4a91c3
Alem Haddush Fitwi <fitwialem2022@fb.com>
2023-02-26 00:15:33 -0800

    Update sqlite.sql

--- sqlite.sql
+++ sqlite.sql
@@ -1,1 +1,3 @@
 -- primary key: UNIQUE and cannot be NULL
+-- Foreign key: used to link two tables together. a field or set of 
+--              field in one table that refers to the PKin another table
